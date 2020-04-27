1f9384fb4b0ac178a8493ff8ad613a51ece239fc
natagoh <natagoh@stanford.edu>
2020-04-27 16:06:41 -0400

    rename react components with .jsx extension

---
+++ nextjs/pages/start-game.jsx
@@ -0,0 +1,12 @@
+import Layout from '../components/PageLayout'
+
+export default function StartGame() {
+  return (
+    <Layout>
+      <h1>Start Game</h1>
+      <p>
+        Start a game now! Copy a link and send it to your friends [link here]
+      </p>
+    </Layout>
+  )
+}
