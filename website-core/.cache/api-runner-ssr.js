ac5b4ee285e8799fec9991ae4e485a76de82fb0a
natagoh <natagoh@stanford.edu>
2020-04-26 20:20:34 -0400

    removed extraneous file

--- website-core/.cache/api-runner-ssr.js
+++ website-core/.cache/api-runner-ssr.js
@@ -1,14 +1,14 @@
 var plugins = [{
-      plugin: require('/Users/z/Desktop/Team-12/website-core/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      plugin: require('C:/Users/gohna/Documents/Spring 2020/CS 194/Team-12/website-core/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/z/Desktop/Team-12/website-core/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('C:/Users/gohna/Documents/Spring 2020/CS 194/Team-12/website-core/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/z/Desktop/Team-12/website-core/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('C:/Users/gohna/Documents/Spring 2020/CS 194/Team-12/website-core/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"Stanford Speakers Bureau","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/194logocropped.png"},
     },{
-      plugin: require('/Users/z/Desktop/Team-12/website-core/gatsby-ssr'),
+      plugin: require('C:/Users/gohna/Documents/Spring 2020/CS 194/Team-12/website-core/gatsby-ssr'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- website-core/.cache/sync-requires.js
+++ website-core/.cache/sync-requires.js
@@ -5,10 +5,11 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/z/Desktop/Team-12/website-core/src/pages/404.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("/Users/z/Desktop/Team-12/website-core/src/pages/about.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("/Users/z/Desktop/Team-12/website-core/src/pages/contact.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/z/Desktop/Team-12/website-core/src/pages/index.js"))),
-  "component---src-pages-startgame-js": hot(preferDefault(require("/Users/z/Desktop/Team-12/website-core/src/pages/startgame.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\gohna\\Documents\\Spring 2020\\CS 194\\Team-12\\website-core\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\gohna\\Documents\\Spring 2020\\CS 194\\Team-12\\website-core\\src\\pages\\404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\gohna\\Documents\\Spring 2020\\CS 194\\Team-12\\website-core\\src\\pages\\about.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("C:\\Users\\gohna\\Documents\\Spring 2020\\CS 194\\Team-12\\website-core\\src\\pages\\contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\gohna\\Documents\\Spring 2020\\CS 194\\Team-12\\website-core\\src\\pages\\index.js"))),
+  "component---src-pages-startgame-js": hot(preferDefault(require("C:\\Users\\gohna\\Documents\\Spring 2020\\CS 194\\Team-12\\website-core\\src\\pages\\startgame.js")))
 }
 
