a1345db4bb79317a5d8ac2954e4a8ab1d225fbac
natagoh <natagoh@stanford.edu>
2020-04-27 15:28:03 -0400

    nextjs starter code

---
+++ nextjs/pages/contact.js
@@ -0,0 +1,12 @@
+import Layout from '../components/PageLayout'
+
+export default function Contact() {
+	return (
+		<Layout>
+			<h1>Contact</h1>
+			<p>
+				Suggestions? Let us know below. (input field maybe idk)
+			</p>
+		</Layout>
+	)
+}
