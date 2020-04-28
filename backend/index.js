6127de2cbaa66e400bd0a0f0141ddafc8377f80d
natagoh <natagoh@stanford.edu>
2020-04-27 23:00:09 -0400

    backend room prototype

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,37 @@
 var express = require('express');
 var app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-const port = 8000	// server port
-// var server = http.createServer(app);
-var server = app.listen(port, () => {
-  console.log(`listening on localhost:${port}`);
+const uuidv4 = require('uuid/v4');
+
+const PORT = 8000	// server port
+
+const rooms = {}
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
 });
-var io = require('socket.io')(server);
-
-// server.get('/', (req, res) => {
-//   res.send('<h1>Hello world</h1>');
-// });
 
 io.on('connection', (socket) => { 
 	console.log("client connected")
 	socket.on('test', test => {
 		console.log("test worked!")
 	})
-	/* … */ 
+	
+	// called when a user wants to create a new room
+	socket.on('createRoom', () => {
+		const id = uuidv4()
+		const [ roomId ] = id.split('-').slice(-1)
+
+		const room = {
+			id: roomId,
+			sockets: []
+		}
+		rooms[room.id] = room
+		joinRoom(socket, room)
+	})
 });
 
 const testAPI = socket => {
@@ -25,4 +39,22 @@ const testAPI = socket => {
   // Emitting a new message. Will be consumed by the client
   socket.emit("test", response);
 };
+
+/**
+ * connects a socket to a specified room
+ * @param {object} [socket] [a connected socket]
+ * @param {object} [room] [object representing a room]
+ */
+const joinRoom = (socket, room) => {
+ 	room.sockets.push(socket);
+  socket.join(room.id, () => {
+    // store the room id in the socket for future use
+    socket.roomId = room.id;
+    console.log(`player ${socket.id} joined room ${room.id}`);
+  });
+}
+
+server.listen(PORT, () => {
+	console.log(`Server is live on PORT:${PORT}`);
+});
 
