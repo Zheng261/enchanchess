075b0d47e23add22fc1cbe2484d481d6c1d3372d
Michelle Huang <huangmjy@stanford.edu>
2020-04-29 03:08:34 -0400

    second test

--- pages/about.jsx
+++ pages/about.jsx
@@ -8,7 +8,7 @@ export default function About() {
 				We're GGEZ and we're trying to help you social distance :) We're unaffiliated with the CAH team and all that.
 				This is an online version of Cards Against Humanity, a party game for horrible people. Rules can be found here [link]
 			</p>
-			<p> Send help. :( </p> 
+			<p> Send help. :( Test #2 </p> 
 		</PageLayout>
 	)
 }
