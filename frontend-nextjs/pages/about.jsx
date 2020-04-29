fb969ec262a96b53460df4937cae58ea547b964f
Zheng <yzh@stanford.edu>
2020-04-28 22:53:02 -0700

    small change to test heroku

--- frontend-nextjs/pages/about.jsx
+++ frontend-nextjs/pages/about.jsx
@@ -6,7 +6,7 @@ export default function About() {
 			<h1>About</h1>
 			<p>			
 				We're GGEZ and we're trying to help you social distance :) We're unaffiliated with the CAH team and all that.
-				This is an online version of Cards Against Humanity, a party game for horrible people. Rules can be found here [link]
+				This is an online version of Cards Against Humanity, a party game for horrible people. Rules can be found <a href="http://s3.amazonaws.com/cah/CAH_Rules.pdf" target="_blank">here</a>.
 			</p>
 		</PageLayout>
 	)
