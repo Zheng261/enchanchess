be0d8878992b37719d18296b240bfc4ebd521cbb
natagoh <natagoh@stanford.edu>
2020-05-10 01:29:49 -0400

    pageLayout -> HeaderLayout rename

---
+++ pages/play-placeholder.jsx
@@ -0,0 +1,11 @@
+import HeaderLayout from '../components/HeaderLayout'
+
+// screen where cards/actual game can be played lives
+// currently a duplicate of GamePlay.jsx in components 
+export default function GamePlay() {
+  return (
+  	<HeaderLayout>
+    	<p>hi</p>
+    </HeaderLayout>
+  )
+}
