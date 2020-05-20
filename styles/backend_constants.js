eca31bd73a586867dd8972d1e6e990453f39ad71
hgui <hgui@stanford.edu>
2020-05-20 01:22:43 -0400

    switched endpoint back

--- styles/backend_constants.js
+++ styles/backend_constants.js
@@ -1,8 +1,8 @@
 // We will put databse keys, etc. here too for consistency
 
 module.exports = {
-    // ENDPOINT: "https://bestcah-api.herokuapp.com/"
-    ENDPOINT: "http://127.0.0.1:8000"
+    ENDPOINT: "https://bestcah-api.herokuapp.com/"
+    // ENDPOINT: "http://127.0.0.1:8000"
 };
 
 //change ENDPOINT: "http://127.0.0.1:8000" for dev
