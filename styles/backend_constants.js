c747c6c4c369a639befc985072d58aeabef4be5d
natagoh <natagoh@stanford.edu>
2020-05-23 14:46:20 -0400

    https

--- styles/backend_constants.js
+++ styles/backend_constants.js
@@ -1,7 +1,7 @@
 // We will put databse keys, etc. here too for consistency
 
 module.exports = {
-    ENDPOINT: "https://bestcah-api.herokuapp.com/"
+    ENDPOINT: "http://bestcah-api.herokuapp.com/"
     //ENDPOINT: "http://127.0.0.1:8000"
 };
 
