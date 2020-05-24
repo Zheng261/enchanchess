78bca113afc88fd9e799603424a2627a7686afaf
natagoh <natagoh@stanford.edu>
2020-05-24 03:37:05 -0400

    no longer have to manually change endpoint

--- styles/backend_constants.js
+++
@@ -1,9 +0,0 @@
-// We will put databse keys, etc. here too for consistency
-
-module.exports = {
-    ENDPOINT: "http://bestcah-api.herokuapp.com/"
-    //ENDPOINT: "http://127.0.0.1:8000"
-};
-
-//change ENDPOINT: "http://127.0.0.1:8000" for dev
-//change ENDPOINT: "https://bestcah-api.herokuapp.com/" for production
