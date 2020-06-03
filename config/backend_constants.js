772f0038db5ca07f86755b51a703d17380a14b22
Zheng <yzh@stanford.edu>
2020-06-03 15:11:56 -0700

    endpoint

--- config/backend_constants.js
+++ config/backend_constants.js
@@ -1,8 +1,8 @@
 // We will put databse keys, etc. here too for consistency
 
 module.exports = {
-   // ENDPOINT: "http://bestcah-api.herokuapp.com/",
-   ENDPOINT: "http://127.0.0.1:8000",
+   ENDPOINT: "http://bestcah-api.herokuapp.com/",
+   //ENDPOINT: "http://127.0.0.1:8000",
 };
 
 // change ENDPOINT: "http://127.0.0.1:8000" for dev
